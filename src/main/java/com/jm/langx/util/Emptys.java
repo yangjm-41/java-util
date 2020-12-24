a03c2f15c08a1f15e42b9513fd1a7152e56484eb
yangjm-41 <932786235@qq.com>
2020-12-24 17:46:06 +0800

    1.完善判空逻辑

--- src/main/java/com/jm/langx/util/Emptys.java
+++ src/main/java/com/jm/langx/util/Emptys.java
@@ -1,5 +1,6 @@
 package com.jm.langx.util;
 
+import java.lang.reflect.Array;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
@@ -15,10 +16,37 @@ public class Emptys {
         return o != null;
     }
 
+    public static final boolean anyEmpty(Object... args){
+        if(Emptys.isEmpty(args)){
+
+        }
+        for (Object arg : args) {
+            if (Emptys.isEmpty(arg)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static final boolean allEmpty(Object... args){
+        for (Object arg : args) {
+            if (Emptys.isNotEmpty(arg)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
     public static final boolean isEmpty(Object o){
         if(o == null){
             return true;
         }
+        if (o.getClass().isArray()) {
+            return Array.getLength(o) <= 0;
+        }
+
         if(o instanceof Collection){
             return ((Collection)o).isEmpty();
         }
@@ -31,10 +59,26 @@ public class Emptys {
         if(o instanceof String){
             return ((String)o).isEmpty();
         }
+        if (o instanceof CharSequence) {
+            CharSequence cs = (CharSequence) o;
+            return cs.length() == 0;
+        }
+
         return false;
     }
 
     public static final boolean isNotEmpty(Object o){
         return !isEmpty(o);
+    }
+
+    public static void main(String[] args) {
+        Object [] adsas = new Object[]{"123","456"};
+
+        System.out.println(Emptys.isEmpty(adsas));
+        System.out.println(Emptys.isEmpty(new Object[]{}));
+        System.out.println(new Object[]{}.getClass());
+
+
+
     }
 }
